fc82c8d78f17d54763c7ec20975252d447426b3a
Raul Bernal <bernal.raul@gmail.com>
2021-08-11 12:36:53 +0200

    Update statesync_client.sh

--- statesync_client.sh
+++ statesync_client.sh
@@ -14,39 +14,53 @@ then
   wget https://raw.githubusercontent.com/BitCannaGlobal/testnet-bcna-cosmos/main/instructions/public-testnet/genesis.json > $HOME/.bcna/config/genesis.json
   # At this moment: config state sync & launch the syncing (all previous config need to be performed) 
 
-
-  RPC1=seed1.bitcanna.io
+  DOMAIN_1=seed1.bitcanna.io
+  NODE1_IP=$(dig $DOMAIN_1 +short)
+  RPC1="http://$NODE1_IP"
   P2P_PORT1=26656
-  RPC2=seed2.bitcanna.io
+  RPC_PORT1=26657
+
+  DOMAIN_2=seed2.bitcanna.io
+  NODE2_IP=$(dig $DOMAIN_2 +short)
+  RPC2="http://$NODE2_IP"
+  RPC_PORT2=16657
   P2P_PORT2=16656
+
+  NODE3_IP=159.65.198.245 
+  RPC3="http://$NODE3_IP"
+  RPC_PORT3=26657
+  P2P_PORT3=26656
 
   INTERVAL=1000
 
-  LATEST_HEIGHT=$(curl -s $RPC1/block | jq -r .result.block.header.height);
+  LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
   BLOCK_HEIGHT=$(($(($LATEST_HEIGHT / $INTERVAL)) * $INTERVAL));
   if [ $BLOCK_HEIGHT -eq 0 ]; then
     echo "Error: Cannot state sync to block 0; Latest block is $LATEST_HEIGHT and must be at least $INTERVAL; wait a few blocks!"
     exit 1
   fi
 
-  TRUST_HASH=$(curl -s "$RPC1/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+  TRUST_HASH=$(curl -s "$RPC1:$RPC_PORT1/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
   if [ "$TRUST_HASH" == "null" ]; then
     echo "Error: Cannot find block hash. This shouldn't happen :/"
     exit 1
   fi
 
-  NODE1_ID=$(curl -s "$RPC1/status" | jq -r .result.node_info.id)
-  NODE2_ID=$(curl -s "$RPC1/status" | jq -r .result.node_info.id)
+  NODE1_ID=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.id)
+  NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
+  NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
+
 
   sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-  s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$RPC1,$RPC2\"| ; \
+  s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"http://$NODE1_IP:$RPC_PORT1,http://$NODE2_IP:$RPC_PORT2,http://$NODE3_IP:$RPC_PORT3\"| ; \
   s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
   s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-  s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:$P2P_PORT1,${NODE2_ID}@${NODE2_IP}:$P2P_PORT2\"|" $HOME/.bcnad/config/config.toml
+  s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2}\"| ; \
+  s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2},${NODE3_ID}@${NODE3_IP}:${P2P_PORT3}\"|" $HOME/.bcna/config/config.toml
 
+  sed -E -i 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"0.01bcna\"/' $HOME/.bcna/config/app.toml
 
   ./bcnad unsafe-reset-all
-  rm -f $HOME/./bcnad/config/addrbook.json
   ./bcnad start
-  echo If your node is synced considerate to create a service file. 
+   echo If your node is synced considerate to create a service file. 
 fi
