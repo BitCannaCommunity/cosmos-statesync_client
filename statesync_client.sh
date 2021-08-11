fb768bad293238562fafccea2387b2f169201d75
Raul Bernal <bernal.raul@gmail.com>
2021-08-11 16:48:28 +0200

    Update statesync_client.sh

--- statesync_client.sh
+++ statesync_client.sh
@@ -16,22 +16,24 @@ then
   curl -s $GENESIS > $HOME/.bcna/config/genesis.json
   # At this moment: config state sync & launch the syncing (all previous config need to be performed) 
 
-  DOMAIN_1=seed1.bitcanna.io
-  NODE1_IP=$(dig $DOMAIN_1 +short)
+  #DOMAIN_1=seed1.bitcanna.io
+  #NODE1_IP=$(dig $DOMAIN_1 +short)
+  NODE1_IP=178.128.247.173
   RPC1="http://$NODE1_IP"
   P2P_PORT1=26656
   RPC_PORT1=26657
 
-  DOMAIN_2=seed2.bitcanna.io
-  NODE2_IP=$(dig $DOMAIN_2 +short)
+  #DOMAIN_2=seed2.bitcanna.io
+  #NODE2_IP=$(dig $DOMAIN_2 +short)
+  NODE2_IP=159.65.198.245
   RPC2="http://$NODE2_IP"
   RPC_PORT2=16657
   P2P_PORT2=16656
 
-  NODE3_IP=159.65.198.245
-  RPC3="http://$NODE3_IP"
-  RPC_PORT3=26657
-  P2P_PORT3=26656
+  #NODE3_IP=159.65.198.245
+  #RPC3="http://$NODE3_IP"
+  #RPC_PORT3=26657
+  #P2P_PORT3=26656
 
   INTERVAL=1000
 
@@ -50,16 +52,17 @@ then
 
   NODE1_ID=$(curl -s "$RPC1:$RPC_PORT1/status" | jq -r .result.node_info.id)
   NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
-  NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
+  #NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
 
 
   sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-  s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"http://$NODE1_IP:$RPC_PORT1,http://$NODE2_IP:$RPC_PORT2,http://$NODE3_IP:$RPC_PORT3\"| ; \
+  s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"http://$NODE1_IP:$RPC_PORT1,http://$NODE2_IP:$RPC_PORT2\"| ; \
   s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
   s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-  s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2},${NODE3_ID}@${NODE3_IP}:${P2P_PORT3}\"|" $HOME/.bcna/config/config.toml
+  s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2}\"| ; \
+  s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"$d6aa4c9f3ccecb0cc52109a95962b4618d69dd3f@seed1.bitcanna.io:26656,23671067d0fd40aec523290585c7d8e91034a771@seed2.bitcanna.io:16656\"|" $HOME/.bcna/config/config.toml
 
-# BTW 59&60 s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2}\"| ; \ 
+ 
   sed -E -i 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"0.01bcna\"/' $HOME/.bcna/config/app.toml
 
   ./bcnad unsafe-reset-all
