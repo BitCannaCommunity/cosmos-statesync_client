51ea24be2a18bc2405069c189d2f6981b4306952
Raul Bernal <bernal.raul@gmail.com>
2021-08-18 09:37:19 +0200

    Update statesync_client.sh

--- statesync_client.sh
+++ statesync_client.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
+# You need config in two peers (avoid seed servers) this values in app.toml:
+#     [state-sync]
+#     snapshot-interval = 1000
+#     snapshot-keep-recent = 10
+# Pruning should be fine tuned also, for this testings is set to nothing
+#     pruning = "nothing"
+
 set -e
+
+# Change for your custom chain
 BINARY="https://github.com/BitCannaGlobal/bcna/releases/download/v0.2-beta/bcnad"
 GENESIS="https://raw.githubusercontent.com/BitCannaGlobal/testnet-bcna-cosmos/main/instructions/public-testnet/genesis.json"
-# Based on the work of Joe (Chorus-One) for Microtick - https://github.com/microtick/bounties/tree/main/statesync
-read -p "BCNA folder, your keys and config will be erased, proced (y/n)? " -n 1 -r
+APP="BCNA: ~/.bcna"
+
+read -p "$APP folder, your keys and config will be erased, proced (y/n)? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   # BitCanna State Sync client config.
@@ -15,21 +26,19 @@ then
   rm -rf $HOME/./bcnad/config/genesis.json #deletes the default created genesis
   curl -s $GENESIS > $HOME/.bcna/config/genesis.json
   
-  #DOMAIN_1=seed1.bitcanna.io 
-  #NODE1_IP=$(dig $DOMAIN_1 +short)
   NODE1_IP="178.128.247.173"
   RPC1="http://$NODE1_IP"
   P2P_PORT1=26656
   RPC_PORT1=26657
 
-  #DOMAIN_2=seed2.bitcanna.io
-  #NODE2_IP=$(dig $DOMAIN_2 +short)
   NODE2_IP="159.65.198.245"
   RPC2="http://$NODE2_IP"
   RPC_PORT2=26657
   P2P_PORT2=26656
 
-  #NODE3_IP=159.65.198.245
+  #If you want to use a third StateSync Server... 
+  #DOMAIN_3=seed1.bitcanna.io     # If you want to use domain names 
+  #NODE3_IP=$(dig $DOMAIN_1 +short
   #RPC3="http://$NODE3_IP"
   #RPC_PORT3=26657
   #P2P_PORT3=26656
@@ -37,7 +46,6 @@ then
   INTERVAL=1000
 
   LATEST_HEIGHT=$(curl -s $RPC1:$RPC_PORT1/block | jq -r .result.block.header.height);
-  #BLOCK_HEIGHT=$(($(($LATEST_HEIGHT / $INTERVAL)) * $INTERVAL));
   BLOCK_HEIGHT=$((($(($LATEST_HEIGHT / $INTERVAL)) -10) * $INTERVAL)); #Mark addition
   
   if [ $BLOCK_HEIGHT -eq 0 ]; then
