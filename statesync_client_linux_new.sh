0fb3ee0e8d664bc4354920e7e788c45bc0da8529
BitCanna <39583369+BitCannaGlobal@users.noreply.github.com>
2022-03-09 16:08:08 +0100

    Only for new peers/validators

--- statesync_client_linux_new.sh
+++ statesync_client_linux_new.sh
@@ -16,16 +16,15 @@ FILE=$(which jq)
  fi
 
 set -e
-export DATE_BACKUP=`date +"%d_%m_%Y-%H_%M"`
 
 # Change for your custom chain
 BINARY="https://github.com/BitCannaGlobal/bcna/releases/download/v.1.3.1/bcnad"
 GENESIS="https://raw.githubusercontent.com/BitCannaGlobal/bcna/main/genesis.json"
 APP="BCNA: ~/.bcna"
-echo "Welcome to the StateSync script. This script will backup your config, move the current .bcna folder to .old_bcna, sync the last state and restore the previous config. 
-You should have a crypted backup of your wallet keys, your node keys and your validator keys, anyway the script will make a clear backup of the last two. Ensure that you can restore your wallet keys if is needed."
+echo "Welcome to the StateSync script. This script will download the last binary and it will sync the last state. DON'T USE WITH A EXISTENT peer/validator config will be erased.
+You should have a crypted backup of your wallet keys, your node keys and your validator keys. Ensure that you can restore your wallet keys if is needed."
 read -p "have you stopped the BCNAD service? CTRL + C to exit or any key to continue..."
-read -p "$APP folder, your keys and config will be erased, a backup will be made, PROCED (y/n)? " -n 1 -r
+read -p "$APP folder, your keys and config WILL BE ERASED, it's ok if you want to build a peer/validator for first time, PROCED (y/n)? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   # BitCanna State Sync client config.
@@ -35,22 +34,14 @@ then
   cd ~
   if [ -d ~/.bcna ];
   then
-    echo "There is a BCNA folder there... taking a backup and moving to .old_bcna"
-    tar cvfz bcna_folder_backup_$DATE_BACKUP.tgz --exclude=".bcna/data/cs.wal" --exclude=".bcna/data/application.db" --exclude=".bcna/data/blockstore.db" --exclude=".bcna/data/evidence.db" --exclude=".bcna/data/snapshots" --exclude=".bcna/data/state.db"   --exclude=".bcna/data/tx_index.db" .bcna/*
-    if [ -d ~/.old_bcna ];
-
-    then 
-      echo Folder .old_bcna exist, will maintain this old folder.
-      rm  -rf .bcna
-    else
-      mv .bcna .old_bcna
-    fi
+    echo "There is a BCNA folder there... if you want sync the data in an existent peer/validator try the script: statesync_linux_with_backup.sh"
+    exit 1
   else
       echo "New installation...."
-      touch ~/.new_installation
   fi
+
   if [ -f ~/bcnad ];
-  then
+   then
     rm -f bcnad #deletes a previous downloaded binary
   fi
   wget -nc $BINARY
@@ -96,7 +87,6 @@ then
   NODE2_ID=$(curl -s "$RPC2:$RPC_PORT2/status" | jq -r .result.node_info.id)
   #NODE3_ID=$(curl -s "$RPC3:$RPC_PORT3/status" | jq -r .result.node_info.id)
 
-
   sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
   s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"http://$NODE1_IP:$RPC_PORT1,http://$NODE2_IP:$RPC_PORT2\"| ; \
   s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
@@ -104,7 +94,7 @@ then
   s|^(persistent_peers[[:space:]]+=[[:space:]]+).*$|\1\"${NODE1_ID}@${NODE1_IP}:${P2P_PORT1},${NODE2_ID}@${NODE2_IP}:${P2P_PORT2}\"| ; \
   s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"d6aa4c9f3ccecb0cc52109a95962b4618d69dd3f@seed1.bitcanna.io:26656,23671067d0fd40aec523290585c7d8e91034a771@seed2.bitcanna.io:26656\"|" $HOME/.bcna/config/config.toml
 
- 
+  sed -E -i 's/enable = true/enable = false/' $HOME/.bcna/config/config.toml
   sed -E -i -s 's/minimum-gas-prices = \".*\"/minimum-gas-prices = \"0.001ubcna\"/' $HOME/.bcna/config/app.toml
 
   ./bcnad unsafe-reset-all
@@ -113,19 +103,6 @@ then
   echo ##################################################################
   sleep 5
   ./bcnad start
-   if [ -f ~/.new_installation ];
-    then
-      rm -f .new_installation
-      sed -E -i 's/enable = true/enable = false/' $HOME/.bcna/config/config.toml
-   else
-   echo 
-   echo Waiting 10 seconds... your backup will be restored with your previous data.... and BCNAD will start again to test it.
-   echo .
-   echo HIT CTLR+C when last block is synced again.
-   sleep 10
-   tar -xzvf bcna_folder_backup_$DATE_BACKUP.tgz
-   fi
-   ./bcnad start
-   echo If your node is synced considerate to create a service file. Be careful, your backup file is not crypted!
-   echo If process was sucessful you can delete .old_bcna
+  echo If your node is synced considerate to create a service file. Be careful, your backup file is not crypted!
+  echo If process was sucessful you can delete .old_bcna
 fi
